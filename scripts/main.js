c875e5aaf9bde8a41e00aedf96b0b95bdfa2af46
Stampycargos <kategadev@gmail.com>
2025-07-03 21:40:00 -0700

    Fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
+
 document.addEventListener('DOMContentLoaded', () => {
     const filterButtons = document.querySelectorAll('.filter-button');
     const courseItems = document.querySelectorAll('.course-item');
