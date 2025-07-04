0540aaf23a9c99a65a3d04a09b732c98b453d15e
Stampycargos <kategadev@gmail.com>
2025-07-03 20:37:57 -0700

    Fixed

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,5 +4,6 @@ const navlinks = document.querySelector('#nav-bar');
 
 navbuttom.addEventListener('click', () => {
     navbuttom.classList.toggle('show');
-    navlinks.classList.toggle('show');
+    navlinks
+    .classList.toggle('show');
 });
