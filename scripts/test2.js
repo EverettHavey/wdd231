2757f8b9dfcbe47dc956f0079d20f7ec5c980a6c
Stampycargos <kategadev@gmail.com>
2025-07-07 00:05:53 -0700

    Fixed

--- scripts/test2.js
+++
@@ -1,7 +0,0 @@
-async function getData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/'); // request
-  const data = await response.json(); // parse the JSON data
-  console.log(data); // temp output test of data response 
-}
-
-getData();
